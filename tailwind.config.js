c49e04cb4a51188229da9b47ec4d6c1e6ce824d3
Ayon <ayonjodder177@gmail.com>
2022-06-07 01:02:28 +0600

    Dependencies installed

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#43B0EF",
+          "secondary": "#F9F9F9",
+          "accent": "#0F1729",
+          "neutral": "#292929",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
